e1c779d1588c86ab62bf1322fa05dc8a23cc7287
jpires11 <joana.pires@epfl.ch>
2024-11-27 21:08:02 +0100

    preprocessing new fonction

--- preprocessing.py
+++ preprocessing.py
@@ -28,3 +28,52 @@ def rotate_train_data(imgs, gt_imgs):
     print ("Number of images: ", len(augmented_imgs))
     print ("Number of groundtruth images: ", len(augmented_gt_imgs))
     return augmented_imgs, augmented_gt_imgs
+
+
+def flip_train_data(imgs, gt_imgs):
+    augmented_imgs = []
+    augmented_gt_imgs = []
+
+    for image, y in zip(imgs, gt_imgs):
+        augmented_imgs.append(np.ascontiguousarray(image))
+        augmented_gt_imgs.append(np.ascontiguousarray(y))
+
+        # Horizontal flip
+        flipped_h = np.ascontiguousarray(np.fliplr(image))
+        augmented_imgs.append(flipped_h)
+        augmented_gt_imgs.append(np.ascontiguousarray(np.fliplr(y)))
+
+        # Vertical flip
+        flipped_v = np.ascontiguousarray(np.flipud(image))
+        augmented_imgs.append(flipped_v)
+        augmented_gt_imgs.append(np.ascontiguousarray(np.flipud(y)))
+
+        # Horizontal and vertical flip
+        flipped_hv = np.ascontiguousarray(np.flipud(np.fliplr(image)))
+        augmented_imgs.append(flipped_hv)
+        augmented_gt_imgs.append(np.ascontiguousarray(np.flipud(np.fliplr(y))))
+
+    print("Number of images after flipping: ", len(augmented_imgs))
+    print("Number of groundtruth images after flipping: ", len(augmented_gt_imgs))
+    return augmented_imgs, augmented_gt_imgs
+
+def adjust_brightness_contrast(imgs, gt_imgs, factor=0.2):
+    augmented_imgs = []
+    augmented_gt_imgs = []
+
+    for image, y in zip(imgs, gt_imgs):
+        augmented_imgs.append(np.ascontiguousarray(image))
+        augmented_gt_imgs.append(np.ascontiguousarray(y))
+
+        # Increase brightness
+        brighter_image = np.clip(image + factor, 0, 1)
+        augmented_imgs.append(np.ascontiguousarray(brighter_image))
+        augmented_gt_imgs.append(np.ascontiguousarray(y))
+
+        # Decrease brightness
+        darker_image = np.clip(image - factor, 0, 1)
+        augmented_imgs.append(np.ascontiguousarray(darker_image))
+        augmented_gt_imgs.append(np.ascontiguousarray(y))
+
+    return augmented_imgs, augmented_gt_imgs
+
