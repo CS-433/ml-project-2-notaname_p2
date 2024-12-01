f8b9b60772354b4f892c08d08704c2e921866d1b
AntoninHUDRY <antonin.hudry@epfl.ch>
2024-12-01 18:01:11 +0100

    added UNet, corrected road_correction, added pipeline to find best parameters

--- experimental_neural_nets.py
+++ experimental_neural_nets.py
@@ -59,7 +59,7 @@ class Conv_test(torch.nn.Module):
             in_channels=in_channels, out_channels=1, kernel_size=1
         )
 
-        self.initialize_weights()
+        #self.initialize_weights()
 
     def initialize_weights(self):
         """Initialize weights non-randomly."""
@@ -82,7 +82,7 @@ class Conv_test(torch.nn.Module):
 
     def forward(self, x):
 
-        indentity = None
+        identity = None
         skip_count = 0
 
         for i, (conv_layer, batchnorm_layer) in enumerate(zip(self.conv_list, self.batchnorm_list)):
@@ -96,7 +96,7 @@ class Conv_test(torch.nn.Module):
                 skip_count += 1
 
             x = batchnorm_layer(x)
-            x = silu(x)
+            x = relu(x)
         
 
         x = self.output_layer(x)
@@ -104,6 +104,123 @@ class Conv_test(torch.nn.Module):
         x = torch.sigmoid(x)
 
         return x
+
+
+class DoubleConv(torch.nn.Module):
+    """
+    Double convolution: Conv -> BatchNorm -> ReLU -> Conv -> BatchNorm -> ReLU
+    """
+    def __init__(self, in_channels, out_channels):
+        super(DoubleConv, self).__init__()
+        self.double_conv = torch.nn.Sequential(
+            torch.nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1, bias=False),
+            torch.nn.BatchNorm2d(out_channels),
+            torch.nn.SiLU(inplace=True),
+            torch.nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1, bias=False),
+            torch.nn.BatchNorm2d(out_channels),
+            torch.nn.SiLU(inplace=True),
+        )
+    
+    def forward(self, x):
+        return self.double_conv(x)
+
+class Down(torch.nn.Module):
+    """
+    Downsampling: MaxPool -> DoubleConv
+    """
+    def __init__(self, in_channels, out_channels):
+        super(Down, self).__init__()
+        self.down = torch.nn.Sequential(
+            torch.nn.MaxPool2d(2),
+            DoubleConv(in_channels, out_channels)
+        )
+    
+    def forward(self, x):
+        return self.down(x)
+
+class Up(torch.nn.Module):
+    """
+    Upsampling: Transposed Conv -> Concat -> DoubleConv
+    """
+    def __init__(self, in_channels, out_channels):
+        super(Up, self).__init__()
+        
+        self.up = torch.nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+        self.conv = DoubleConv(in_channels, out_channels)
+    
+    def forward(self, x1, x2):
+        x1 = self.up(x1)
+        # In case of dimension missmatch
+        diff_y = x2.size(2) - x1.size(2)
+        diff_x = x2.size(3) - x1.size(3)
+        x1 = torch.nn.functional.pad(x1, [diff_x // 2, diff_x - diff_x // 2,
+                                    diff_y // 2, diff_y - diff_y // 2])
+
+                                
+        x1 = self.conv(x1)
+        x = torch.cat([x2, x1], dim=1)
+        return self.conv(x)
+
+class OutConv(torch.nn.Module):
+    """
+    Output Convolution: Conv -> Activation (optional)
+    """
+    def __init__(self, in_channels, out_channels):
+        super(OutConv, self).__init__()
+        self.conv = torch.nn.Conv2d(in_channels, out_channels, kernel_size=1)
+    
+    def forward(self, x):
+        x = self.conv(x)
+        x = torch.squeeze(x)
+        x = torch.sigmoid(x)
+        return x
+
+class UNet(torch.nn.Module):
+    """
+    U-Net Model with flexible number of layers
+    """
+    def __init__(self, n_channels=3, base_c=64, num_layers=4):
+        super(UNet, self).__init__()
+
+        self.n_channels = n_channels
+        self.num_layers = num_layers
+
+        # Encoder
+        self.inc = DoubleConv(n_channels, base_c)
+        self.downs = torch.nn.ModuleList()  # To store the down-sampling layers
+        in_channels = base_c
+        print('down')
+        for i in range(num_layers):
+            out_channels = in_channels * 2
+            self.downs.append(Down(in_channels, out_channels))
+            print((in_channels,out_channels))
+            in_channels = out_channels
+
+        # Decoder
+        self.ups = torch.nn.ModuleList()  # To store the up-sampling layers
+        print('Up')
+        for i in range(num_layers):
+            out_channels = in_channels // 2
+            self.ups.append(Up(in_channels, out_channels))
+            print((in_channels,out_channels))
+            in_channels = out_channels
+
+        # Output
+        self.outc = OutConv(base_c, 1)
+
+    def forward(self, x):
+        x1 = self.inc(x)
+        encodings = [x1]
+        for down in self.downs:
+            encodings.append(down(encodings[-1]))
+
+        x = encodings[-1]
+        for i, up in enumerate(self.ups):
+            x = up(x, encodings[-i-2])
+
+        x = self.outc(x)
+        return x
+
 
 class Road_data(Dataset):
     def __init__(self, images, labels):
--- road_correction.py
+++ road_correction.py
@@ -11,10 +11,18 @@ from skimage.measure import label, regionprops
 '''
 
 
-def remove_false_positives(binary_mask, min_aspect_ratio=2.5):
+def remove_false_positives(binary_mask, min_aspect_ratio=2.5, min_area=400):
     """
     Remove false positives such as houses, outlier pixels, or small structures.
     Now uses max/min ratios for both width/length and length/width to better handle crossing roads.
+    
+    Args:
+        binary_mask (ndarray): Binary mask of the image.
+        min_aspect_ratio (float): Minimum aspect ratio (major/minor axis) to retain regions.
+        min_area (int): Minimum area for a region to be retained.
+        
+    Returns:
+        ndarray: Binary mask with false positives removed.
     """
     labeled_mask = label(binary_mask)
     filtered_mask = np.zeros_like(binary_mask, dtype=bool)
@@ -25,37 +33,35 @@ def remove_false_positives(binary_mask, min_aspect_ratio=2.5):
         minor_axis = region.minor_axis_length
         
         if minor_axis == 0:
-            aspect_ratio = 1  # Prevent division by zero
+            aspect_ratio = float("inf")  # Prevent division by zero
         else:
             aspect_ratio = major_axis / minor_axis
 
-        # Retain components with a high aspect ratio (long and narrow objects)
-        if aspect_ratio >= min_aspect_ratio:
+        # Check aspect ratio and area
+        if aspect_ratio >= min_aspect_ratio or region.area >= min_area:
             filtered_mask[labeled_mask == region.label] = True
 
     return filtered_mask
 
 
-def process_roads(raw_map, threshold=0.5, min_size=50, min_aspect_ratio=2.5, outlier_size=20, display=False):
+
+def process_roads(raw_map, threshold=0.05, outlier_size=50, shape_size=5, min_aspect_ratio=2.5, display=False):
     """
-    Post-process the CNN output to straighten roads, dynamically estimate widths, and remove false positives
+    Post-process the CNN output to straighten roads and remove false positives
     and outliers *after* reconstructing roads.
     """
     # Transform input into binary
     binary_road_map = (raw_map > threshold).astype(np.uint8)
-
+    
     # Remove small pixel objects
-    cleaned_mask = remove_small_objects(binary_road_map > 0, min_size=min_size)
+    cleaned_mask = remove_small_objects(binary_road_map > 0, min_size=outlier_size)
 
     # Fill small holes with closing (square + diamond)
-    filled_map = closing(cleaned_mask, square(3))
-    filled_map = closing(filled_map, diamond(3))
+    filled_map = closing(cleaned_mask, square(shape_size))
+    filled_map = closing(filled_map, diamond(shape_size))
 
     # Remove false positives using aspect ratio
     filtered_map = remove_false_positives(filled_map, min_aspect_ratio=min_aspect_ratio)
-    
-    # Remove small outliers again after the processing
-    final_map = remove_small_objects(filtered_map, min_size=outlier_size)
     
     if display:
         fig, axes = plt.subplots(1, 5, figsize=(15, 5))
@@ -78,3 +84,32 @@ def process_roads(raw_map, threshold=0.5, min_size=50, min_aspect_ratio=2.5, out
         plt.show()
 
     return final_map
+
+def f1_loss_numpy(predictions, targets, epsilon=1e-7):
+    """
+    Compute F1 loss using NumPy.
+
+    Args:
+        predictions (np.ndarray): Predicted probabilities (float values between 0 and 1).
+        targets (np.ndarray): Ground truth binary labels (0 or 1).
+        threshold (float): Threshold to binarize predictions.
+        epsilon (float): Small constant to avoid division by zero.
+
+    Returns:
+        float: F1 loss (1 - F1 score).
+    """
+
+    # Compute true positives, false positives, false negatives
+    true_positive = np.sum(predictions * targets)
+    false_positive = np.sum(predictions * (1 - targets))
+    false_negative = np.sum((1 - predictions) * targets)
+
+    # Compute precision and recall
+    precision = true_positive / (true_positive + false_positive + epsilon)
+    recall = true_positive / (true_positive + false_negative + epsilon)
+
+    # Compute F1 score
+    f1_score = 2 * (precision * recall) / (precision + recall + epsilon)
+
+    # F1 loss is 1 - F1 score
+    return 1 - f1_score
